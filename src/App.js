0ae7fb64cb8113def8c69db7b870baab5597bb19
AdityaJasuja <47072645+AdityaJasuja@users.noreply.github.com>
2019-11-16 22:47:50 -0800

    adding Redux and Thunk
    Created a dummy database, added redux to create reducers and thunk as the middle ground for dispatching the actions.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './components/dashboard/Dashboard.js'
 import ProjectDetails from './components/projects/ProjectDetails.js'
 import SignIn from './components/auth/SignIn.js'
 import SignUp from './components/auth/SignUp.js'
+import CreateProject from './components/projects/CreateProject.js'
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path='/project/:id' component={ProjectDetails} />
           <Route path='/signin' component={SignIn} />
           <Route path='/signup' component={SignUp} />
+          <Route path='/create' component={CreateProject} />
         </Switch>
       </div>
     </BrowserRouter>
